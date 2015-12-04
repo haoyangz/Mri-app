4dc46c539738f22dfd3869891a05cb416fe562ab
Haoyang Zeng <zenghaoyang@gmail.com>
2015-12-04 13:54:12 -0500

    Trace testing loss rather than training loss

--- mriapp/utilities/line_parser.py
+++ mriapp/utilities/line_parser.py
@@ -7,7 +7,8 @@ from future import standard_library
 standard_library.install_aliases()
 import re
 
-_reg_loss = re.compile(r'\bIteration\s\d+, loss\s=\s[0-9\.]+\b')
+_reg_loss = re.compile(r'\bTest\snet\soutput\s#\d: loss\s=\s[0-9\.]+\b')
+#_reg_loss = re.compile(r'\bIteration\s\d+, loss\s=\s[0-9\.]+\b')
 _reg_acc = re.compile(r'accuracy\s=\s[0-9\.]+', flags=re.DOTALL)
 _reg_iter = re.compile(r'Iteration\s\d+')
 
@@ -34,8 +35,8 @@ def parse_caffe_train_line(line):
     if iteration_match:
         training_event['iteration'] = int(iteration_match[0].replace('Iteration ', ''))
     if loss_match:
-        iteration_str, loss_str = loss_match[0].split(',')
-        training_event['loss'] = float(loss_str.replace('loss = ', ''))
+        iteration_str, loss_str = loss_match[0].split(':')
+        training_event['loss'] = float(loss_str.replace(' loss = ', ''))
     elif acc_match:
         training_event['accuracy'] = float(acc_match[0].replace('accuracy = ', ''))
     return training_event
